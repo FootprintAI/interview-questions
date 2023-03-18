b92df2b15c953b98142783c844f2167fbb1990c7
root <root@X542UN.localdomain>
2023-03-19 00:44:27 +0800

    <feat> DRF's Routers

--- quiz/01-rate-limit/quiz01/core/models.py
+++ quiz/01-rate-limit/quiz01/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Music(models.Model):
+class CoreModel(models.Model):
     song = models.TextField()
     singer = models.TextField()
     last_modify_date = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = "music"
+    # class Meta:
+    #     db_table = "coremodel"
--- quiz/01-rate-limit/quiz01/quiz01/urls.py
+++ quiz/01-rate-limit/quiz01/quiz01/urls.py
@@ -18,10 +18,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from core.views import CoreViewSet
 
+
 router = DefaultRouter()
-# router.register('core', CoreViewSet)
+router.register('core', CoreViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    # for rest_framework
+    path('api/', include(router.urls)),
+    # for rest_framework auth
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
