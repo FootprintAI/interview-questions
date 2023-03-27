8e4f150de9d3c38e30640b5ac772745720a08a19
dirtyrabbit <s8888863@gmail.com>
2023-03-27 22:13:17 +0800

    <test> add execution time

--- quiz/01-rate-limit/quiz01/core/tests.py
+++ quiz/01-rate-limit/quiz01/core/tests.py
@@ -13,36 +13,46 @@ class APITestCase(TestCase):
     def test_get_not_over(self):
         print('-----------Get not over-----------')
         resp = self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
+        t0 = time.time()
         for i in range(1,101):
             resp = self.c.get('/api/')
+            print(resp.content)
+        print('execution time: '+str(time.time()-t0))
         self.assertEqual(resp.status_code, 200)
-    
+        
+    # Over rate limiting get test 
+    # will be get 429 status
+    def test_get_over100(self):
+        print('-----------Get over-----------')
+        self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
+        t0 = time.time()
+        for i in range(1,200):
+            resp = self.c.get('/api/')
+            print(resp.content)
+        print('execution time: '+str(time.time()-t0))
+        self.assertEqual(resp.status_code, 429)
 
     # Unover rate limiting post test 
     # will be get 200 status
     def test_post_not_over(self):
         print('-----------Post not over-----------')
         self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
+        t0 = time.time()
         resp = self.c.post('/api/')
+        print(resp.content)
+        print('execution time: '+str(time.time()-t0))
         self.assertEqual(resp.status_code, 200)
-
-    # Over rate limiting get test 
-    # will be get 429 status
-    
-    def test_get_over100(self):
-        print('-----------Get over-----------')
-        self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
-        for i in range(1,102):
-            resp = self.c.get('/api/')
-        self.assertEqual(resp.status_code, 429)
 
     # # Over rate limiting post test 
     # # # will be get 429 status
     def test_post_over1(self):
         print('-----------Post over-----------')
         resp = self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
+        t0 = time.time()
         resp = self.c.post('/api/')
+        print(resp.content)
         resp = self.c.post('/api/')
-        resp = self.c.post('/api/')
+        print(resp.content)
+        print('execution time: '+str(time.time()-t0))
         self.assertEqual(resp.status_code, 429)    
 
