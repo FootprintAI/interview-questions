f3bb7c65bb045dd87477855c0e495ae2d47ac9d2
dirtyrabbit <s8888863@gmail.com>
2023-03-28 13:27:04 +0800

    <feat> test time greate 1s loop

--- quiz/01-rate-limit/quiz01/core/tests.py
+++ quiz/01-rate-limit/quiz01/core/tests.py
@@ -8,51 +8,138 @@ class APITestCase(TestCase):
     def setUp(self):
         self.c = client.Client()
 
-    # Unover rate limiting get test 
-    # will be get 200 status
+    # # Unover rate limiting get test 
+    # # will be get 200 status
     def test_get_not_over(self):
         print('-----------Get not over-----------')
-        resp = self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
-        t0 = time.time()
-        for i in range(1,101):
-            resp = self.c.get('/api/')
-            print(resp.content)
-        print('execution time: '+str(time.time()-t0))
-        self.assertEqual(resp.status_code, 200)
         
-    # Over rate limiting get test 
-    # will be get 429 status
+        # If the execution time is greater than 1 second, 
+        # we must retest until it is less than 1 second or has been executed 5 times  
+        for i in range(1,5):
+            #reset api limit
+            resp = self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'}) 
+            t0 = int(time.time())
+            #number of request
+            for j in range(1,101):
+                resp = self.c.get('/api/') #request api
+
+                # get diff header file by status code
+                if(resp.status_code == 200):
+                    print('[X-RateLimit-Limit]: '+ resp['X-RateLimit-Limit'] 
+                        + ' [X-RateLimit-Remaining]: '+ resp['X-RateLimit-Remaining'] 
+                        + ' [X-RateLimit-Reset]: '+ resp['X-RateLimit-Reset'])
+                elif(resp.status_code == 429):
+                    print('[Retry-At]: '+ resp['Retry-At'] )
+            t1 = int(time.time())
+
+            # if execution time < 1, don't do again
+            if((t1-t0) <1):
+                break
+        print('execution time: '+str(t1-t0))
+
+        if((t1-t0) >1):
+            self.assertEqual(0, 1) #we can't get it for less than 1s
+        else:
+            self.assertEqual(resp.status_code, 200)
+        
+    # # Over rate limiting get test 
+    # # will be get 429 status
     def test_get_over100(self):
         print('-----------Get over-----------')
-        self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
-        t0 = time.time()
-        for i in range(1,102):
-            resp = self.c.get('/api/')
-            print(resp.content)
-        print('execution time: '+str(time.time()-t0))
-        self.assertEqual(resp.status_code, 429)
+        
 
-    # Unover rate limiting post test 
-    # will be get 200 status
+        # If the execution time is greater than 1 second, 
+        # we must retest until it is less than 1 second or has been executed 5 times 
+        for i in range(1,5):
+            #reset api limit
+            self.c.post('/reset/',{'group':'get', 'key':'ip', 'rate':'100/s', 'method':'GET'})
+            t0 = int(time.time())
+
+            #number of request
+            for j in range(1,102):
+                resp = self.c.get('/api/') #request api
+
+                # get diff header file by status code
+                if(resp.status_code == 200):
+                    print('[X-RateLimit-Limit]: '+ resp['X-RateLimit-Limit'] 
+                        + ' [X-RateLimit-Remaining]: '+ resp['X-RateLimit-Remaining'] 
+                        + ' [X-RateLimit-Reset]: '+ resp['X-RateLimit-Reset'])
+                elif(resp.status_code == 429):
+                    print('[Retry-At]: '+ resp['Retry-At'] )
+            t1 = int(time.time())
+
+            # if execution time < 1, don't do again
+            if((t1-t0) <1):
+                break
+
+        print('execution time: '+str(t1-t0))
+        if((t1-t0) >1):
+            self.assertEqual(0, 1) #can't done for less than 1s
+        else:
+            self.assertEqual(resp.status_code, 429) 
+
+
+    # # Unover rate limiting post test 
+    # # will be get 200 status
     def test_post_not_over(self):
         print('-----------Post not over-----------')
-        self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
-        t0 = time.time()
-        resp = self.c.post('/api/')
-        print(resp.content)
-        print('execution time: '+str(time.time()-t0))
-        self.assertEqual(resp.status_code, 200)
+        for i in range(1,5):
+            self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
+            t0 = int(time.time())
+            resp = self.c.post('/api/') #request api
+
+            # get diff header file by status code
+            if(resp.status_code == 200):
+                print('[X-RateLimit-Limit]: '+ resp['X-RateLimit-Limit'] 
+                    + ' [X-RateLimit-Remaining]: '+ resp['X-RateLimit-Remaining'] 
+                    + ' [X-RateLimit-Reset]: '+ resp['X-RateLimit-Reset'])
+            elif(resp.status_code == 429):
+                print('[Retry-At]: '+ resp['Retry-At'] )
+            t1 = int(time.time())
+
+            # if execution time < 1, don't do again
+            if((t1-t0) <1):
+                break
+
+        print('execution time: '+str(t1-t0))
+        if((t1-t0) >1):
+            self.assertEqual(0, 1) #can't done for less than 1s
+        else:
+            self.assertEqual(resp.status_code, 200) 
 
     # # Over rate limiting post test 
-    # # # will be get 429 status
+    # # will be get 429 status
     def test_post_over1(self):
         print('-----------Post over-----------')
-        resp = self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
-        t0 = time.time()
-        resp = self.c.post('/api/')
-        print(resp.content)
-        resp = self.c.post('/api/')
-        print(resp.content)
-        print('execution time: '+str(time.time()-t0))
-        self.assertEqual(resp.status_code, 429)    
+        for i in range(1,5):
+            resp = self.c.post('/reset/',{'group':'post', 'key':'ip', 'rate':'1/s', 'method':'POST'})
+            t0 = int(time.time())
+            resp = self.c.post('/api/') #request api
+
+            # get diff header file by status code
+            if(resp.status_code == 200):
+                print('[X-RateLimit-Limit]: '+ resp['X-RateLimit-Limit'] 
+                    + ' [X-RateLimit-Remaining]: '+ resp['X-RateLimit-Remaining'] 
+                    + ' [X-RateLimit-Reset]: '+ resp['X-RateLimit-Reset'])
+            elif(resp.status_code == 429):
+                print('[Retry-At]: '+ resp['Retry-At'] )
+            resp = self.c.post('/api/')
+            if(resp.status_code == 200):
+                print('[X-RateLimit-Limit]: '+ resp['X-RateLimit-Limit'] 
+                    + ' [X-RateLimit-Remaining]: '+ resp['X-RateLimit-Remaining'] 
+                    + ' [X-RateLimit-Reset]: '+ resp['X-RateLimit-Reset'])
+            elif(resp.status_code == 429):
+                print('[Retry-At]: '+ resp['Retry-At'] )
+
+            t1 = int(time.time())
+
+            # if execution time < 1, don't do again
+            if((t1-t0) <1):
+                break
+
+        print('execution time: '+str(t1-t0))
+        if((t1-t0) >1):
+            self.assertEqual(0, 1) #can't done for less than 1s
+        else:
+            self.assertEqual(resp.status_code, 429)    
 
